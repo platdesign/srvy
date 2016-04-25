51479905f20cc926b9a4b96081ff1d23fed41d3a
Christian Blaschke <mail@platdesign.de>
2016-04-25 16:57:04 +0200

    cleaned

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 'use strict';
 
-var HTTPClient = require('./lib/HTTPClient');
-
-
-
-
-var client = new HTTPClient('sugar');
-
-
-client.request('GET').then((res)=>{
-	console.log('res', res);
-}, (err)=>{
-	console.log('err', err);
-});
+return require('./lib');
